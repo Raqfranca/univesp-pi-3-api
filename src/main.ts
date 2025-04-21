e83fa1442c91e24817d9ba1a4ce72e3f48481ba2
raqfranca <raqfranca@hotmail.com>
2025-04-21 12:20:59 -0300

    Change app.listen

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //await app.listen(process.env.PORT ?? 3000);
-  await app.listen(8080, '127.0.0.1');
+  await app.listen(3000, '0.0.0.0');
+  //await app.listen(8080, '127.0.0.1');
 
-  console.log('Application is running on http://127.0.0.1:8080');
+  //console.log('Application is running on http://127.0.0.1:8080');
 }
 bootstrap();
+
+
+
+
